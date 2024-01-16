634a17416ef222ec41a92d95d9ac669b62af4df3
Moon-YJ <tunjeong@gmail.com>
2024-01-16 22:07:21 +0900

    useText custom hook 추가

--- src/components/headerNav/HeaderNav.jsx
+++ src/components/headerNav/HeaderNav.jsx
@@ -3,15 +3,17 @@ import clsx from 'clsx';
 import styles from './headerNav.module.scss';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
+import { useCustomText } from '@/hooks/useText';
 
 export default function HeaderNav({ pages }) {
 	const pathName = usePathname();
+	const customTxt = useCustomText('combined');
 
 	return (
 		<nav className={clsx(styles.headerNav)}>
 			{pages.map(page => (
 				<Link key={page} href={`/${page}`} className={clsx(pathName === `/${page}` ? styles.on : '')}>
-					{page}
+					{customTxt(page)}
 				</Link>
 			))}
 		</nav>
