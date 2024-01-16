268723be74ec662bf13ca5505c1f212a8ff93ff8
Moon-YJ <tunjeong@gmail.com>
2024-01-16 21:11:47 +0900

    init

---
+++ next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const path = require('path');
+const nextConfig = {
+	reactStrictMode: false,
+	sassOptions: {
+		includePath: [path.join(__dirname, 'styles')],
+		prependData: `@import '@/styles/variables.scss';`
+	}
+};
+
+module.exports = nextConfig;
---
+++ src/app/page.js
@@ -0,0 +1,9 @@
+import styles from './main.module.scss';
+
+export default function Home() {
+	return (
+		<main className={styles.main}>
+			<h1>Main</h1>
+		</main>
+	);
+}
