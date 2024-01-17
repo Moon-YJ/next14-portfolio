9ccc69e1acc3fc4fd3ee0a66379dc7160df19102
Moon-YJ <tunjeong@gmail.com>
2024-01-17 15:09:23 +0900

    mongoDB 접속 함수 추가

---
+++ src/lib/connectDB.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+let isConnected = false;
+
+export const connectDB = async () => {
+	try {
+		if (isConnected) return console.log('Already Connected');
+		const db = await mongoose.connect(process.env.MONGO_URL);
+		isConnected = db.connection[0].readyState;
+	} catch (error) {
+		console.log(error);
+		throw new Error('Connection to the DB Failed');
+	}
+};
