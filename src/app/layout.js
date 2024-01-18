50619ce8e0865327aa5243a078755395c316ea78
Moon-YJ <tunjeong@gmail.com>
2024-01-18 22:44:11 +0900

    header에 로그인 토글 버튼 작업

--- src/app/layout.js
+++ src/app/layout.js
@@ -13,8 +13,10 @@ export default function RootLayout({ children }) {
 	return (
 		<html lang='en'>
 			<body className={inter.className}>
-				<Header />
-				<div className='container'>{children}</div>
+				<div className='container'>
+					<Header />
+					<div className='inner'>{children}</div>
+				</div>
 			</body>
 		</html>
 	);
--- src/components/headerNav/HeaderNav.jsx
+++ src/components/headerNav/HeaderNav.jsx
@@ -4,18 +4,28 @@ import styles from './headerNav.module.scss';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import { useCustomText } from '@/hooks/useText';
+import { useState } from 'react';
 
 export default function HeaderNav({ pages }) {
 	const pathName = usePathname();
+	const [Checked, setChecked] = useState(false);
 	const customTxt = useCustomText('combined');
+	const handleClick = () => {
+		setChecked(!Checked);
+	};
 
 	return (
-		<nav className={clsx(styles.headerNav)}>
-			{pages.map(page => (
-				<Link key={page} href={`/${page}`} className={clsx(pathName === `/${page}` ? styles.on : '')}>
-					{customTxt(page)}
-				</Link>
-			))}
-		</nav>
+		<>
+			<nav className={clsx(styles.headerNav)}>
+				{pages.map(page => (
+					<Link key={page} href={`/${page}`} className={clsx(pathName === `/${page}` ? styles.on : '')}>
+						{customTxt(page)}
+					</Link>
+				))}
+				<div className={clsx(`${styles.switch} ${Checked ? styles.checked : ''}`)} onClick={handleClick}>
+					<div className={clsx(styles.ball)}></div>
+				</div>
+			</nav>
+		</>
 	);
 }
