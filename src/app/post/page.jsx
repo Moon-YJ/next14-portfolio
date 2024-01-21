4de4f58d3324144d6e7174a25b0e7eeba9c505da
Moon-YJ <tunjeong@gmail.com>
2024-01-21 23:37:34 +0900

    mongoDB post 데이터 호출(SSR방식)

--- src/app/post/page.jsx
+++ src/app/post/page.jsx
@@ -1,10 +1,21 @@
 import clsx from 'clsx';
 import styles from './post.module.scss';
+import { getPosts } from '@/lib/actions';
+import Image from 'next/image';
 
-export default function Post() {
+export default async function Post() {
+	const posts = await getPosts();
+
 	return (
-		<div className={clsx(styles.post)}>
+		<section className={clsx(styles.post)}>
 			<h1>Post</h1>
-		</div>
+			{posts.map(post => (
+				<article key={post._id}>
+					<div className={clsx(styles.pic)}>{post.img && <Image src={post.img} alt={post.title} priority fill />}</div>
+					<h2>{post.title}</h2>
+					<p>{post.desc}</p>
+				</article>
+			))}
+		</section>
 	);
 }
